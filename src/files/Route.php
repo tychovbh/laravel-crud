3bb5ee3ef1b1bc903e183e07ed7c4eb94670beed
Tycho <info@bespokeweb.nl>
2022-04-06 15:25:21 +0200

    remove edit and create (not used yet) from routes

--- src/files/Route.php
+++ src/files/Route.php
@@ -15,9 +15,7 @@ class Name implements Routes
     {
         Route::get('/{models}', [Controller::class, 'index'])->name('{models}.index');
         Route::get('/{models}/{id}', [Controller::class, 'show'])->name('{models}.show');
-        Route::get('/{models}/create', [Controller::class, 'create'])->name('{models}.create');
         Route::post('/{models}', [Controller::class, 'store'])->name('{models}.store');
-        Route::get('/{models}/{id}/edit', [Controller::class, 'edit'])->name('{models}.edit');
         Route::put('/{models}/{id}', [Controller::class, 'update'])->name('{models}.update');
         Route::delete('/{models}/{id}', [Controller::class, 'destroy'])->name('{models}.destroy');
     }
