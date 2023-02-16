56a947d13c3de6e7377280376299afaa92ba31de
Tycho <info@bespokeweb.nl>
2023-02-16 14:44:14 +0100

    support crud bindings for project controllers that extend the crud controller

--- src/Middleware/CrudBindings.php
+++ src/Middleware/CrudBindings.php
@@ -5,15 +5,15 @@ namespace Tychovbh\LaravelCrud\Middleware;
 use Closure;
 use Exception;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use Tychovbh\LaravelCrud\Actions\ModelName;
 use Tychovbh\LaravelCrud\Actions\ModelNamespace;
 use Tychovbh\LaravelCrud\Actions\ModelQuery;
 use Tychovbh\LaravelCrud\Actions\RequestMethod;
+use ReflectionClass;
+use Tychovbh\LaravelCrud\Controller;
 
 class CrudBindings
 {
@@ -27,15 +27,33 @@ class CrudBindings
      */
     public function handle(Request $request, Closure $next): mixed
     {
-
         $action = $request->route()->getAction();
-        if (Str::contains($action['controller'], 'Tychovbh\LaravelCrud\Controller')) {
-            $model = (new ModelName())->get($request);
-            Route::bind(Str::lower($model), fn(int $id) => $this->findModel($model, $id));
-            Route::bind('id', fn(int $id) => $this->findModel($model, $id));
+        $controller = explode('@', $action['controller'])[0];
+
+        if ($controller === Controller::class) {
+            $this->bindModel($request);
+            return $next($request);
+        }
+
+        $class = new ReflectionClass($controller);
+        $parent = $class->getParentClass();
+
+        if ($parent->name === Controller::class) {
+            $this->bindModel($request);
         }
 
         return $next($request);
+    }
+
+    /**
+     * Bind model
+     * @param Request $request
+     */
+    private function bindModel(Request $request)
+    {
+        $model = (new ModelName())->get($request);
+        Route::bind(Str::lower($model), fn(int $id) => $this->findModel($model, $id));
+        Route::bind('id', fn(int $id) => $this->findModel($model, $id));
     }
 
     /**
